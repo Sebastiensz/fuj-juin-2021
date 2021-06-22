5faa403d4e70b97f07d8179904f7d024c28a0443
Sebastien Szollosi <sszollosi@esrifrance.fr>
2021-06-22 12:20:24 +0200

    dev : added publicDir

--- back/server.js
+++ back/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const serveIndex = require("serve-index");
 const app = express();
-const port = 3000;
+const port = process.env.ORSYS_PORT || 3000;
+const publicDir = process.env.ORSYS_WWDIR || "./public";
 
 //app.get("/", (req, res) => {
 //  res.send("Hello World!");
 //});
 
-app.use(express.static(".")); //fct qui renvoie fct est une fct de 1er ordre
-app.use(serveIndex("."));
+app.use(express.static(publicDir)); //fct qui renvoie fct est une fct de 1er ordre
+app.use(serveIndex(publicDir));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
