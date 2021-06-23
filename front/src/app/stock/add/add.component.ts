da8f028493d1542dd857d968c626074d8c4fe7b1
Sebastien Szollosi <sebastien.szollosi@gmail.com>
2021-06-23 16:23:49 +0100

    dev: service router and article

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('Pince', [Validators.required]),
+    price: new FormControl(15, [Validators.required]),
+    qty: new FormControl(1, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submit() {
+    console.log('coucou');
+    // qdzdz.add(this.f.value as Article);
+    this.router.navigateByUrl('/stock');
   }
-
 }
